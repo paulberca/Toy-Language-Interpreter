5aa343dbb99cf957b2bb72c51576206ec8ec2fe0
paulb <paulberca@gmail.com>
2024-11-08 22:42:00 +0200

    added the TextTable class along with Commands and the Interface

--- src/repo/Repository.java
+++ src/repo/Repository.java
@@ -7,16 +7,6 @@ import model.exception.MyException;
 import model.prgstate.*;
 
 public class Repository implements IRepo {
-//    private List<PrgState> prgList;
-//
-//    public Repository(List<PrgState> prgList) {
-//        this.prgList = prgList;
-//    }
-//
-//    public PrgState getCrtPrg() {
-//        return prgList.getLast();
-//    }
-
     private PrgState prg;
     private String logFilePath;
 
